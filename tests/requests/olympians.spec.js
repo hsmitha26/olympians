29fd487e953f68db91d38570da0a7356380d0378
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2020-01-13 22:14:21 -0700

    Add test file for api/v1/olympians endpoint

---
+++ tests/requests/olympians.spec.js
@@ -0,0 +1,7 @@
+var shell = require('shelljs');
+var request = require("supertest");
+var app = require('../../app');
+
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../../knexfile')[environment];
+const database = require('knex')(configuration);
