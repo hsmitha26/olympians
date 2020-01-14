195a44384bab812c6beaf4cf9766ed0d6c7a3485
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2020-01-13 22:52:35 -0700

    Add initial test for endpoint

--- tests/requests/olympians.spec.js
+++ tests/requests/olympians.spec.js
@@ -5,3 +5,33 @@ var app = require('../../app');
 const environment = process.env.NODE_ENV || 'test';
 const configuration = require('../../knexfile')[environment];
 const database = require('knex')(configuration);
+
+describe('Test GET api/v1/olympians', () => {
+  beforeEach(async () => {
+     await database.raw('truncate table olympians cascade');
+   })
+    it('should get all olympians', async() => {
+      const res = await request(app).get("/api/v1/olympians")
+
+      expect(res.statusCode).toBe(200)
+
+      expect(res.body).toHaveProperty('olympians')
+
+      // console.log(res.body)
+      // expect(res.body.olympians[0]).toHaveProperty('name')
+
+      // expect(res.body.olympians[0]).toHaveProperty('team')
+      // expect(res.body.olympians[0]).toBe("Australia")
+    })
+
+    xit('should generate error message for sad path', async() => {
+      const res = await request(app).get("/api/v1/olympians")
+
+      expect(res.statusCode).toBe(404)
+      expect(res.body.error).toBe("No olympians found")
+    })
+
+  afterEach(() => {
+    database.raw('truncate table olympians cascade');
+  });
+});
